f8593ad2136d2fd879b0e3bafb3b3f28fa944f7c
PedroVidalDev <pedrohvidals@gmail.com>
2025-03-13 12:14:15 -0300

    feat: create LocalsCreate component with form for adding and editing locals

---
+++ src/pages/LocalsCreate/index.tsx
@@ -0,0 +1,170 @@
+import { useTranslation } from 'react-i18next'
+import { useCallback, useEffect } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { useNavigate, useParams } from 'react-router-dom'
+
+import { Input } from 'components/Input'
+import { Button } from 'components/Button'
+
+import { REGEX } from 'constants/regex'
+
+import { useAlert } from 'hooks/useAlert'
+import { useLocal } from 'hooks/useLocal'
+import { useGroup } from 'hooks/useGroup'
+
+import { CreateLocalFormData } from './types'
+
+import {
+  FooterActionsContainer,
+  ButtonActions,
+  Container,
+  ContainerForm,
+  ContainerFormAbout,
+  ContainerFormMain,
+  ContainerHeader,
+  FormText,
+  FormTitle,
+  Title,
+} from './styles'
+
+export const LocalsCreate = () => {
+  const { id } = useParams()
+
+  const { t } = useTranslation()
+
+  const navigate = useNavigate()
+
+  const { fetchGetAllGroups, groupPageData } = useGroup()
+
+  const {
+    fetchCreateLocal,
+    fetchGetLocalById,
+    fetchDeleteLocalById,
+    fetchUpdateLocal,
+  } = useLocal()
+
+  const { alert } = useAlert()
+
+  const isEditing = !!id && window.location.pathname.includes('/edit/')
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<CreateLocalFormData>({
+    defaultValues: {
+      name: '',
+      mac: '',
+      groups: [],
+    },
+  })
+
+  const fillLocalFields = useCallback(async () => {
+    try {
+      const groupData = await fetchGetLocalById(id as string)
+
+      if (groupData) {
+        setValue('name', groupData.name)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }, [fetchGetLocalById, id, setValue])
+
+  const handleDelete = async () => {
+    alert({
+      onConfirm: onConfirmDelete,
+      title: 'Deletar Local',
+      message: 'Deseja realmente deletar este local?',
+    })
+  }
+
+  const onConfirmDelete = async () => {
+    await fetchDeleteLocalById(id as string)
+  }
+
+  const handleCancel = () => {
+    navigate('/locals')
+  }
+
+  const onSubmit = async (data: CreateLocalFormData) => {
+    if (isEditing) {
+      await fetchUpdateLocal(Number(id), data)
+    } else {
+      await fetchCreateLocal(data)
+    }
+  }
+
+  useEffect(() => {
+    fetchGetAllGroups()
+  }, [fetchGetAllGroups])
+
+  useEffect(() => {
+    if (isEditing) {
+      fillLocalFields()
+    }
+  }, [isEditing, id, fillLocalFields])
+
+  return (
+    <Container>
+      <ContainerHeader>
+        <Title> {t('title.locals')} </Title>
+        <ButtonActions>
+          <Button
+            onClick={handleCancel}
+            text={t('button.cancel')}
+            color="ERROR_LIGHT"
+          />
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            text={isEditing ? t('button.update') : t('button.create')}
+            color="PRIMARY_LIGHT"
+          />
+        </ButtonActions>
+      </ContainerHeader>
+      <ContainerFormMain>
+        <ContainerFormAbout>
+          <FormTitle> Dados do local </FormTitle>
+          <FormText>
+            Nesse formulário será possível cadastrar o local, para isso preencha
+            as informações e clique no botão de cadastrar. Lembrando que os MACs
+            no select são os disponíveis na sua rede.
+          </FormText>
+        </ContainerFormAbout>
+
+        <ContainerForm>
+          <Controller
+            control={control}
+            name="name"
+            rules={{
+              required: t('inputErrors.required'),
+              pattern: {
+                value: REGEX.onlyString,
+                message: t('inputErrors.text'),
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                value={value}
+                onChange={onChange}
+                placeholder={t('placeholder.default')}
+                label="Nome"
+                errorMessage={errors.name?.message}
+              />
+            )}
+          />
+        </ContainerForm>
+      </ContainerFormMain>
+      <FooterActionsContainer>
+        {isEditing && (
+          <Button
+            onClick={handleDelete}
+            text={t('button.delete')}
+            color="ERROR_MAIN"
+          />
+        )}
+      </FooterActionsContainer>
+    </Container>
+  )
+}
---
+++ src/pages/LocalsCreate/types.ts
@@ -0,0 +1,5 @@
+export type CreateLocalFormData = {
+  name: string
+  mac: string
+  groups: number[]
+}
