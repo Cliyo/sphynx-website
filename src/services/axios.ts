e93a10be3e9091bdf17c5839aaef142110d39c9e
PedroVidalDev <pedrohvidals@gmail.com>
2025-03-12 22:54:32 -0300

    feat: add error notification handling in axios response interceptor

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 import { BASE_URL } from 'constants/request'
+import { notify } from 'utils/notification'
 
 export const api = axios.create({
   baseURL: BASE_URL,
@@ -8,3 +9,23 @@ export const api = axios.create({
     language: 'pt-BR',
   },
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    const { response } = error
+
+    if (response) {
+      const data = response.data
+      notify(data.message ?? 'Ocorreu um erro na operação', 'error')
+    } else {
+      notify('Ocorreu um erro na operação', 'error')
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export default api
