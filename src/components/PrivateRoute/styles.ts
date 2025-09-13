22b3faba2cce6c52410524f6622088090b28e819
Pedro Henrique Vidal dos Santos <113215138+PedroVidalDev@users.noreply.github.com>
2025-09-13 09:19:05 -0300

    Add files via upload
    feat: add style for private route

---
+++ src/components/PrivateRoute/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PrivateRouteContainer = styled.div`
+    position: relative;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+
+    padding-left: 320px;
+`
