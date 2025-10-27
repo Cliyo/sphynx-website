0c5819b4ca128dcbc1d873bc073fa50383074e02
Snootic <snooticonoob@gmail.com>
2025-10-26 21:06:51 -0300

    fix: update regex for onlyString to allow any character

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,5 +1,5 @@
 export const REGEX = {
-  onlyString: /^[A-Za-zÀ-ÖØ-öø-ÿçãÇÃ0-9 ]+$/,
+  onlyString: /^.*$/,
   onlyNumbers: /^\d+$/g,
   email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
 }
